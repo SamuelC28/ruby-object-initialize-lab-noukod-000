3fa9126996c20441f6e936556d37fe2ed6e51392
Samuel Cornet <samuel@noukod.com>
2018-09-05 13:09:39 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,29 @@
+class Person
+
+  def name=(name)
+     @name = name
+  end
+ def name
+    @name
+  end
+
+  def job=(job)
+     @job = job
+  end
+ def job
+    @job
+  end
+end
+=======
+  def person (person)
+     @person = person
+  end
+
+  def person=(person)
+    @person
+  end
+end
+
+beyonce = Person.new
+beyonce.name = "Beyonce"
+>>>>>>> 186cbe85fa047d3adbded7f5ee75f6fe41944785
